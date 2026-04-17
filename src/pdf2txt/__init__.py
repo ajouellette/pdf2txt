2dffebcc2e7cc0e12f53854a47331500183068bd
Aaron Ouellette <aaronjo2@illinois.edu>
2026-04-17 13:41:38 -0500

    reorganization to expose script and library function

--- src/pdf2txt/__init__.py
+++ src/pdf2txt/__init__.py
@@ -1,6 +1,3 @@
-from . import cli
 from .reader import read_pdf
 
-
-def main() -> None:
-    cli.main()
+__all__ = ["read_pdf"]
--- src/pdf2txt/cli.py
+++ src/pdf2txt/cli.py
@@ -2,10 +2,12 @@ import argparse
 
 from .reader import read_pdf
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("PDF")
     args = parser.parse_args()
+
+    # extract and print to stdout
     full_text = read_pdf(args.PDF)
     print(full_text)
-
