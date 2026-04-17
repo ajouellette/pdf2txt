dfa3646201fe5915963f66dd3a10022a087c4050
Aaron Ouellette <aaronjo2@illinois.edu>
2026-04-17 14:05:23 -0500

    add option to extract a specific page range

--- src/pdf2txt/cli.py
+++ src/pdf2txt/cli.py
@@ -1,13 +1,44 @@
 import argparse
+import re
 
 from .reader import read_pdf
+
+
+def parse_page_range(page_range: str) -> tuple[int, int]:
+    """Parse a page range, ensuring that it is valid."""
+    pattern = r"^(\d+)-(\d+)$"
+    match = re.match(pattern, page_range)
+    if match is not None:
+        pages = (int(match.group(1)), int(match.group(2)))
+    else:
+        raise ValueError("Invalid page range format. Must be given as START-END.")
+
+    if pages[0] < 1:
+        raise ValueError(
+            "Invalid page range. START must be greater than or equal to 1."
+        )
+    if pages[0] > pages[1]:
+        raise ValueError(
+            "Invalid page range. END must be greater than or equal to START."
+        )
+
+    return pages
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("PDF")
+    parser.add_argument(
+        "-p",
+        "--pages",
+        default=None,
+        type=str,
+        help="Page range (inclusive) to extract. Must be given as START-END.",
+    )
     args = parser.parse_args()
 
+    pages = parse_page_range(args.pages) if args.pages else None
+
     # extract and print to stdout
-    full_text = read_pdf(args.PDF)
+    full_text = read_pdf(args.PDF, page_range=pages)
     print(full_text)
--- src/pdf2txt/reader.py
+++ src/pdf2txt/reader.py
@@ -1,10 +1,41 @@
 from pypdf import PdfReader
 
 
-def read_pdf(fname: str) -> str:
-    """Extract text from a pdf file."""
+def read_pdf(
+    fname: str, page_range: None | tuple[int | None, int | None] = None
+) -> str:
+    """
+    Extract text from a pdf file.
+
+    Args:
+        fname (str): Path to the pdf file.
+        page_range (None | tuple[int | None, int | None]): Optional page range to extract. Page range is given as a tuple (start, end) and is inclusive.
+
+    Returns:
+        str: The extracted text.
+
+    Assumptions:
+        - Start and end of page range are greater than zero.
+        - End of page range is greater than start.
+
+    Raises:
+        RuntimeError: If the page range exceeds the number of pages in the document.
+    """
     pages = []
     reader = PdfReader(fname)
-    for page in reader.pages:
+    if page_range is not None:
+        start = page_range[0] - 1 if page_range[0] is not None else 0
+        end = page_range[1] if page_range[1] is not None else len(reader.pages)
+        # these conditions should not occur:
+        assert start >= 0 and end >= 0
+        assert end > start
+        if start >= len(reader.pages) or end > len(reader.pages):
+            raise RuntimeError(
+                f"Asked for an invalid page range {page_range}. Document only has {len(reader.pages)} pages."
+            )
+        page_slice = slice(start, end)
+    else:
+        page_slice = slice(0, len(reader.pages))
+    for page in reader.pages[page_slice]:
         pages.append(page.extract_text())
     return "\n".join(pages)
